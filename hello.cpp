f71f810703fcaba5ec545e3d24a87b1a69f2d90b
zhanghe-github <zhanghe@cqu.edu.cn>
2014-11-08 23:15:33 +0800

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,6 @@ using namespace std;
 
 int main()
 {
-	cout<<"hello world github edited by centos7 edited by GitHub"<<endl;
+	cout<<"hello world github edited by centos7 edited by GitHub2"<<endl;
 return 0;
 }
